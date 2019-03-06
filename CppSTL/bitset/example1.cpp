28a8403ccc4a1cac2d5a3e7289c78b57b550cea9
ayushgupta2959 <37008291+ayushgupta2959@users.noreply.github.com>
2019-03-06 19:14:29 +0530

    created bitset/example1

---
+++ CppSTL/bitset/example1.cpp
@@ -0,0 +1,10 @@
+#include<bits\stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n = 45;
+    bitset<32> bn(n);
+    cout<<bn;
+    return 0;
+}

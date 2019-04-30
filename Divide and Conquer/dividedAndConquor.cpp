278715689b8b79689363f7b48be559b654589f0c
ayushgupta2959 <37008291+ayushgupta2959@users.noreply.github.com>
2019-04-30 22:54:19 +0530

    Create dividedAndConquor.cpp

---
+++ Divide and Conquer/dividedAndConquor.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;++i) cin>>a[i];
+    return 0;
+}
